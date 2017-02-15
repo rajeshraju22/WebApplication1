0adfe75d22d988c0e0671232f35b5a4a358daf54
rajeshraju22 <rajeshraju.it22@gmail.com>
2017-02-15 23:16:06 +0530

    assignments
    session2assignment1

---
+++ WebApplication1/s2a1 script.js
@@ -0,0 +1,22 @@
+﻿function employees(sourceID, destID)
+{
+    var src = document.getElementById(sourceID);
+    var dest = document.getElementById(destID);
+    for (var count = 0; count < src.options.length; count++)
+    {
+        if (src.options[count].selected == true)
+        {
+            var option = src.options[count];
+            var newOption = document.createElement("option");
+            newOption.value = option.value; newOption.text = option.text;
+            newOption.selected = true; try
+            {
+                dest.add(newOption, null);
+                src.remove(count, null);
+            } catch (error) {
+                dest.add(newOption); src.remove(count);
+            }
+            count--;
+        }
+    }
+}
